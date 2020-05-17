53201521c2e392b0e0077e9ac7b4457813aa4b52
Julien Colombain <julien@colombain.com>
2020-05-17 18:05:08 +0200

    layout

--- simulation.py
+++ simulation.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
+def get_data_from_json():
+  input_file = open('data.json')
+  return json.load(input_file)
+
+json_array = get_data_from_json()
 
 @app.route('/')
 @app.route('/home')
 def home():
-  return "<h1>Flask app home page</h1>"
+  return render_template('home.html')
+
+# TODO: Remove this after adding data
+@app.route('/posts')
+def posts():
+  return render_template('posts.html', posts=json_array["posts"])
 
 @app.route('/about')
 def about():
-  return "<h1>About Page</h1>"
+  return render_template('about.html')
+
+@app.route('/data')
+def data():
+  return json_array
