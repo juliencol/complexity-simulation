42d44432bae7fe679bcaf52c5c12adb01ed87527
Julien Colombain <julien@colombain.com>
2020-05-19 16:49:11 +0200

    removed fake data

--- simulation.py
+++ simulation.py
@@ -4,13 +4,6 @@ import json, random
 
 app = Flask(__name__)
 
-def get_data_from_json():
-  input_file = open('data.json')
-  return json.load(input_file)
-
-json_array = get_data_from_json()
-posts = json_array['posts']
-
 @app.route('/', methods=["GET", "POST"])
 @app.route('/somme1', methods=["GET", "POST"])
 def sum1():
@@ -19,10 +12,6 @@ def sum1():
 @app.route('/somme2')
 def sum2():
   return render_template('sum2.html')
-
-@app.route('/data')
-def data():
-  return json_array
 
 if __name__ == "__main__":
   app.run()
